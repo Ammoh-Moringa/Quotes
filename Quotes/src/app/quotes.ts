013512b3fb1300131ed8ef7d0267706e05f34ca0
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-05-14 16:30:03 +0300

    Add quote component

--- Quotes/src/app/quotes.ts
+++ Quotes/src/app/quotes.ts
@@ -1,6 +1,15 @@
 export class Quotes {
   showInfo: boolean;
-  constructor(public id:number,public name:string,public title:string,public quote:string,public author:string, public datePosted: Date,public likes:number, public dislikes:number){
-      this.showInfo=false;
-    }
+  constructor(
+    public id: number,
+    public name: string,
+    public title: string,
+    public quote: string,
+    public author: string,
+    public datePosted: Date,
+    public likes: number,
+    public dislikes: number
+  ) {
+    this.showInfo = false;
+  }
 }
