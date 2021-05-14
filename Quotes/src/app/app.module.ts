fcbdb0743cde26664e742f25d4624f095fad577a
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-05-14 22:00:57 +0300

    Add highlight directive

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuotesFormsComponent } from './quotes-forms/quotes-forms.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    QuotesFormsComponent
+    QuotesFormsComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
